103390916c4ac656209779b4153d7c4df87e002b
ManasiDhotkar <99855497+ManasiDhotkar@users.noreply.github.com>
2023-08-19 00:21:12 +0530

    Time: 5 ms (57.14%), Space: 41.9 MB (91.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
-        
-        return Arrays.equals(sArray, tArray);
+       int arr[] = new int[26]; //26 alphabets
+       if(s.length() != t.length())
+       {
+           return false;
+       }
+       for(int i=0;i<s.length();i++)
+       {
+           arr[s.charAt(i) - 'a'] ++; // adding
+           arr[t.charAt(i) - 'a']--; // subtracting 
+       }
+      for(int i=0;i<arr.length;i++)
+      {
+          if(arr[i] != 0) // if non zero occur in array
+          {
+              return false;
+          }
+      }
+       return true;
     }
 }
