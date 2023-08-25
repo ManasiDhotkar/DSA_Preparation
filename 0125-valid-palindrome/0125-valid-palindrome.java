bbbc4002d94144438a8388c73deb06d121bf8681
ManasiDhotkar <99855497+ManasiDhotkar@users.noreply.github.com>
2023-08-25 16:13:24 +0530

    Time: 17 ms (44.86%), Space: 43.6 MB (57.11%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        if(s.equals(sb.toString()))
+        {
+            return true;
+        }
+        return false;  
+        }
+      
+}
